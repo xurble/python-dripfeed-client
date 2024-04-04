b05ccf5d147149974a061286d998a066fda61a48
Gareth Simpson <g@xurble.org>
2024-04-04 07:36:27 +0100

    Version bump

--- changelog.md
+++ changelog.md
@@ -1,3 +1,5 @@
+### 0.1.4 -- 4th April 2024
+- Add `get_or_add_feed` helper method
 
 ### 0.1.0 -> 0.1.2 -- 3rd April 2024
 - Resolve packaging issues
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setuptools.setup(
     name='dripfeed-client',
-    version='0.1.2',
+    version='0.1.3',
     description='A client for the dripfeed RSS proxy service.',
     long_description=long_description,
     long_description_content_type='text/markdown',
